768aa21faf6e7df10307cd8056282b5488cae8c7
feliciaszhang <feliciaszhang@gmail.com>
2019-11-06 16:03:58 -0500

    adjust buildpack

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import nltk
+import os
 from nltk.tokenize import word_tokenize
 from nltk.corpus import wordnet
 
@@ -55,4 +56,5 @@ def syn():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
